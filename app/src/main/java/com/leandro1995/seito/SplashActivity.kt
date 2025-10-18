2090d5f8c23beef4d1fb5c26ac328be52ad04912
LEANDRO\leccb <Leandro>
2025-10-18 02:15:05 -0500

    Implementar installSplashScreen

--- app/src/main/java/com/leandro1995/seito/SplashActivity.kt
+++ app/src/main/java/com/leandro1995/seito/SplashActivity.kt
@@ -3,11 +3,14 @@ package com.leandro1995.seito
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().setKeepOnScreenCondition { true }
     }
 }
