091cadefe8f1807bced5be629dd28d7c993148de
LEANDRO\leccb <Leandro>
2025-10-18 02:41:29 -0500

    Configurar temporizador para splash

--- app/src/main/java/com/leandro1995/seito/SplashActivity.kt
+++ app/src/main/java/com/leandro1995/seito/SplashActivity.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.leandro1995.seito.background.coroutine.BackGroundCoroutine
+import com.leandro1995.seito.background.coroutine.setting.TypeTimeCoroutine
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
@@ -12,5 +14,15 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         installSplashScreen().setKeepOnScreenCondition { true }
+
+        BackGroundCoroutine(
+            time = TIME_SKIP, typeTimeCoroutine = TypeTimeCoroutine.SECONDS
+        ).start {
+
+        }
+    }
+
+    companion object {
+        private const val TIME_SKIP = 5L
     }
 }
