4b87fa84b3abdf76cf04fe3cb35fd36aae4bfdb0
LEANDRO\leccb <Leandro>
2025-10-18 02:17:50 -0500

    Implementar libreria de coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.splashscreen)
+    implementation(libs.kotlinx.coroutines.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
