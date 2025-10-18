b92ae6b6a85613bc5a3e5590f8660accccef2749
LEANDRO\leccb <Leandro>
2025-10-18 05:02:48 -0500

    Detectar si est habilidado el gesto o no en el telefono

--- app/src/main/java/com/leandro1995/seito/activity/ambient/ActivityAmbient.kt
+++ app/src/main/java/com/leandro1995/seito/activity/ambient/ActivityAmbient.kt
@@ -2,11 +2,13 @@ package com.leandro1995.seito.activity.ambient
 
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 open class ActivityAmbient : AppCompatActivity() {
 
@@ -16,16 +18,33 @@ open class ActivityAmbient : AppCompatActivity() {
     }
 
     private fun fullScreen() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.BAKLAVA) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.BAKLAVA && !isGestureNavigation()) {
             enableEdgeToEdge()
             ViewCompat.setOnApplyWindowInsetsListener(
                 findViewById<ViewGroup>(android.R.id.content).getChildAt(
                     0
-                )) { v, insets ->
+                )
+            ) { v, insets ->
                 val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
                 v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
                 insets
             }
         }
+    }
+
+    fun isGestureNavigation(): Boolean {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            return false
+        }
+        
+        return try {
+            Settings.Secure.getInt(this.contentResolver, NAVIGATION_MODE) == 2
+        } catch (_: Settings.SettingNotFoundException) {
+            false
+        }
+    }
+
+    companion object {
+        private const val NAVIGATION_MODE = "navigation_mode"
     }
 }
