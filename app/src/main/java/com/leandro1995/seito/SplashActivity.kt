320d1bb422642ffbbe5e1a994509554c2eefe0c3
LEANDRO\leccb <Leandro>
2025-10-18 00:45:07 -0500

    Eliminar layout de main activity y cambiarle el nombre por splash

---
+++ app/src/main/java/com/leandro1995/seito/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.leandro1995.seito
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
