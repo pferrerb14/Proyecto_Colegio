0b6671debbc1fd853d221097d4c3248ad43fc117
LEANDRO\leccb <Leandro>
2025-10-19 04:11:49 -0500

    Crear base para viewModel

---
+++ app/src/main/java/com/leandro1995/seito/viewmodel/ambient/ViewModelAmbient.kt
@@ -0,0 +1,34 @@
+package com.leandro1995.seito.viewmodel.ambient
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+open class ViewModelAmbient<A, E> : ViewModel() {
+    
+    private val uiAction: MutableStateFlow<A?> by lazy {
+        MutableStateFlow(null)
+    }
+
+    private val uiEvent: MutableSharedFlow<E?> by lazy {
+        MutableSharedFlow()
+    }
+
+    val action = uiAction.asStateFlow()
+
+    val event = uiEvent.asSharedFlow()
+
+    protected val actionButton = fun(action: Int) { event(action = action) }
+
+    protected fun value(action: A) {
+        uiAction.value = action
+    }
+
+    protected fun emit(event: E) {
+        uiEvent.tryEmit(event)
+    }
+
+    protected open fun event(action: Int) {}
+}
