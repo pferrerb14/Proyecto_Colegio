5ff1a616f2fcd66e2c2e4ae23346722085263661
LEANDRO\leccb <Leandro>
2025-10-20 03:18:41 -0500

    Eliminar espaciado estra

--- app/src/main/java/com/leandro1995/seito/intent/event/LoginIntentEvent.kt
+++ app/src/main/java/com/leandro1995/seito/intent/event/LoginIntentEvent.kt
@@ -4,7 +4,6 @@ sealed class LoginIntentEvent {
     data object StudentSelect : LoginIntentEvent()
     data object TeacherSelect : LoginIntentEvent()
     data object AdminSelect : LoginIntentEvent()
-
     data class AlertMessage(val alertMessage: com.leandro1995.seito.model.design.AlertMessage) :
         LoginIntentEvent()
 }
