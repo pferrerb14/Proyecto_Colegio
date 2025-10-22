c7eccf606eb763af38ac73e7cb7774526fe27031
LEANDRO\leccb <Leandro>
2025-10-22 17:51:05 -0500

    Implementar función de ejecución con loading

--- app/src/main/java/com/leandro1995/seito/component/loading/LoadingComponent.kt
+++ app/src/main/java/com/leandro1995/seito/component/loading/LoadingComponent.kt
@@ -3,11 +3,20 @@ package com.leandro1995.seito.component.loading
 import android.content.Context
 import android.util.AttributeSet
 import com.leandro1995.seito.R
+import com.leandro1995.seito.background.coroutine.BackGroundCoroutine
+import com.leandro1995.seito.background.coroutine.setting.TimeTypeCoroutine
 import com.leandro1995.seito.component.ambient.ComponentAmbient
+import com.leandro1995.seito.component.util.NetworkUtil
 import com.leandro1995.seito.databinding.ComponentLoadingBinding
+import com.leandro1995.seito.model.design.AlertMessage
+import com.leandro1995.seito.util.dialog.AppUtilDialog
 
 class LoadingComponent(context: Context, attrs: AttributeSet? = null) :
     ComponentAmbient<ComponentLoadingBinding>(context, attrs) {
+
+    private val networkUtil = NetworkUtil(context = context)
+    private val backGroundCoroutine =
+        BackGroundCoroutine(time = TIME_LONG, timeTypeCoroutine = TimeTypeCoroutine.SECONDS)
 
     override var idLayout: Int = R.layout.component_loading
 
@@ -15,7 +24,26 @@ class LoadingComponent(context: Context, attrs: AttributeSet? = null) :
         onCreateView()
     }
 
+    fun start(method: suspend () -> Unit) {
+        if (!networkUtil.isInternetAvailable()) {
+            visibility(isVisible = true)
+            backGroundCoroutine.start {
+                method()
+                visibility(isVisible = false)
+            }
+        } else {
+            AppUtilDialog.dialogMaterialDesign(
+                context = context,
+                alertMessage = AlertMessage(idMessage = R.string.internet_connection_message)
+            )
+        }
+    }
+
     override fun visibility(isVisible: Boolean) {
         dataBinding?.loadingConstraint?.visibility = if (isVisible) VISIBLE else GONE
+    }
+
+    companion object {
+        private const val TIME_LONG = 5L
     }
 }
--- app/src/main/java/com/leandro1995/seito/model/design/AlertMessage.kt
+++ app/src/main/java/com/leandro1995/seito/model/design/AlertMessage.kt
@@ -1,6 +1,5 @@
 package com.leandro1995.seito.model.design
 
-import android.content.Context
 import androidx.annotation.StringRes
 import com.leandro1995.seito.model.design.ambient.Message
 
