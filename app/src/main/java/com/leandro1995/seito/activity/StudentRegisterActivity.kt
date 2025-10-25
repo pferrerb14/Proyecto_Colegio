c18803888111d14f36ff38c22baeb3c7950830de
LEANDRO\leccb <Leandro>
2025-10-25 18:26:19 -0500

    Eliminar importanciones sin uso

--- app/src/main/java/com/leandro1995/seito/activity/StudentRegisterActivity.kt
+++ app/src/main/java/com/leandro1995/seito/activity/StudentRegisterActivity.kt
@@ -1,10 +1,5 @@
 package com.leandro1995.seito.activity
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.leandro1995.seito.R
 import com.leandro1995.seito.activity.ambient.ActivityAmbient
 import com.leandro1995.seito.databinding.ActivityStudentRegisterBinding
