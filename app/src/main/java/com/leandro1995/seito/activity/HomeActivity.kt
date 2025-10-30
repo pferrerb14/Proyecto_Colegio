53b35c1736056c9be1013b17ac402dc3ec99194e
LEANDRO\leccb <Leandro>
2025-10-30 02:36:52 -0500

    Implementar navegacon de las secciones mencionadas

--- app/src/main/java/com/leandro1995/seito/activity/HomeActivity.kt
+++ app/src/main/java/com/leandro1995/seito/activity/HomeActivity.kt
@@ -1,5 +1,7 @@
 package com.leandro1995.seito.activity
 
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.leandro1995.seito.R
 import com.leandro1995.seito.activity.ambient.ActivityAmbient
 import com.leandro1995.seito.databinding.ActivityHomeBinding
@@ -7,4 +9,12 @@ import com.leandro1995.seito.databinding.ActivityHomeBinding
 class HomeActivity : ActivityAmbient<ActivityHomeBinding>() {
 
     override var idLayout: Int = R.layout.activity_home
+
+    override fun initView() {
+        dataBinding?.menuBottomNavigationView?.setupWithNavController(
+            (supportFragmentManager.findFragmentById(
+                R.id.home_student_fragment
+            ) as NavHostFragment).navController
+        )
+    }
 }
