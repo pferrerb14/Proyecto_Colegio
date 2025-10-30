1a0edf6fb5b8fd348c2d09c682be7ecbb7bb06d9
LEANDRO\leccb <Leandro>
2025-10-29 23:34:11 -0500

    Aplicar el ambiente en home

--- app/src/main/java/com/leandro1995/seito/activity/HomeActivity.kt
+++ app/src/main/java/com/leandro1995/seito/activity/HomeActivity.kt
@@ -1,21 +1,10 @@
 package com.leandro1995.seito.activity
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.leandro1995.seito.R
+import com.leandro1995.seito.activity.ambient.ActivityAmbient
+import com.leandro1995.seito.databinding.ActivityHomeBinding
 
-class HomeActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+class HomeActivity : ActivityAmbient<ActivityHomeBinding>() {
+
+    override var idLayout: Int = R.layout.activity_home
 }
