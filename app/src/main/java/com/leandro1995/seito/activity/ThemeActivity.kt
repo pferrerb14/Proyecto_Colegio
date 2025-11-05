ce3e5dbab667204aa88f4fb87b7093ec1648cc01
LEANDRO\leccb <Leandro>
2025-11-05 05:31:03 -0500

    Mostrar titulo de la ventana tema

--- app/src/main/java/com/leandro1995/seito/activity/ThemeActivity.kt
+++ app/src/main/java/com/leandro1995/seito/activity/ThemeActivity.kt
@@ -3,8 +3,21 @@ package com.leandro1995.seito.activity
 import com.leandro1995.seito.R
 import com.leandro1995.seito.activity.ambient.ActivityAmbient
 import com.leandro1995.seito.databinding.ActivityThemeBinding
+import com.leandro1995.seito.model.design.Toolbar
 
 class ThemeActivity : ActivityAmbient<ActivityThemeBinding>() {
 
     override var idLayout: Int = R.layout.activity_theme
+
+    override fun initView() {
+        dataBinding?.apply {
+            Toolbar(
+                context = this@ThemeActivity,
+                materialToolbar = appBarBlueInclude.toolbar,
+                idTitle = R.string.theme_title,
+                icArrow = R.drawable.ic_arrow_white,
+                isArrow = true
+            ).config { finish() }
+        }
+    }
 }
