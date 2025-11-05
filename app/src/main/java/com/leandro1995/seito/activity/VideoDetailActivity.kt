be4d0263e8e6df27b9cd048c71d808612b29f69a
LEANDRO\leccb <Leandro>
2025-11-04 21:50:50 -0500

    Mostrar titulo en video detalle

--- app/src/main/java/com/leandro1995/seito/activity/VideoDetailActivity.kt
+++ app/src/main/java/com/leandro1995/seito/activity/VideoDetailActivity.kt
@@ -3,8 +3,21 @@ package com.leandro1995.seito.activity
 import com.leandro1995.seito.R
 import com.leandro1995.seito.activity.ambient.ActivityAmbient
 import com.leandro1995.seito.databinding.ActivityVideoDetailBinding
+import com.leandro1995.seito.model.design.Toolbar
 
 class VideoDetailActivity : ActivityAmbient<ActivityVideoDetailBinding>() {
 
     override var idLayout: Int = R.layout.activity_video_detail
+
+    override fun initView() {
+        dataBinding?.apply {
+            Toolbar(
+                this@VideoDetailActivity,
+                appBarBlueInclude.toolbar,
+                R.string.educational_videos_title,
+                true,
+                icArrow = R.drawable.ic_arrow_white
+            ).config { finish() }
+        }
+    }
 }
