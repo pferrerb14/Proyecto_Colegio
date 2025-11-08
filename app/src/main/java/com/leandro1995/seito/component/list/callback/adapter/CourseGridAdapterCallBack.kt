796a5589db9b49f14d02d0e4dccf29641396e520
LEANDRO\leccb <Leandro>
2025-11-08 02:56:51 -0500

    Eliminar importanciones sin uso

--- app/src/main/java/com/leandro1995/seito/component/list/callback/adapter/CourseGridAdapterCallBack.kt
+++ app/src/main/java/com/leandro1995/seito/component/list/callback/adapter/CourseGridAdapterCallBack.kt
@@ -1,7 +1,6 @@
 package com.leandro1995.seito.component.list.callback.adapter
 
 import com.leandro1995.seito.component.list.model.Course
-import com.leandro1995.seito.component.list.model.Theme
 
 interface CourseGridAdapterCallBack {
 
