17d190b2ee5a2b128c80f026136cc73eae261305
LEANDRO\leccb <Leandro>
2025-11-09 20:16:14 -0500

    Implemendar viewmodel basico

--- app/src/main/java/com/leandro1995/seito/fragment/CourseListFragment.kt
+++ app/src/main/java/com/leandro1995/seito/fragment/CourseListFragment.kt
@@ -1,10 +1,18 @@
 package com.leandro1995.seito.fragment
 
+import androidx.fragment.app.viewModels
 import com.leandro1995.seito.R
 import com.leandro1995.seito.databinding.FragmentCourseListBinding
 import com.leandro1995.seito.fragment.ambient.FragmentAmbient
+import com.leandro1995.seito.viewmodel.CourseListViewModel
 
 class CourseListFragment : FragmentAmbient<FragmentCourseListBinding>() {
 
+    private val courseListViewModel by viewModels<CourseListViewModel>()
+
     override var idLayout: Int = R.layout.fragment_course_list
+
+    override fun initView() {
+        dataBinding?.courseListViewModel = courseListViewModel
+    }
 }
