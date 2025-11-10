ed1e83df71460f1fd753166282ad06c156f4ee9d
LEANDRO\leccb <Leandro>
2025-11-10 03:30:05 -0500

    Mostrar pantalla de agregar temas

--- app/src/main/java/com/leandro1995/seito/fragment/CourseListFragment.kt
+++ app/src/main/java/com/leandro1995/seito/fragment/CourseListFragment.kt
@@ -1,9 +1,12 @@
 package com.leandro1995.seito.fragment
 
+import android.content.Intent
 import androidx.fragment.app.viewModels
 import com.leandro1995.seito.R
+import com.leandro1995.seito.activity.CourseAddActivity
 import com.leandro1995.seito.component.list.callback.CourseGridComponentListCallBack
 import com.leandro1995.seito.component.model.Loading
+import com.leandro1995.seito.config.Setting
 import com.leandro1995.seito.databinding.FragmentCourseListBinding
 import com.leandro1995.seito.extension.lifecycleScope
 import com.leandro1995.seito.fragment.ambient.FragmentAmbient
@@ -65,6 +68,8 @@ class CourseListFragment : FragmentAmbient<FragmentCourseListBinding>(),
     }
 
     override fun course(course: Course) {
-
+        startActivity(Intent(requireContext(), CourseAddActivity::class.java).apply {
+            putExtra(Setting.COURSE_PUT_EXTRA, course)
+        })
     }
 }
