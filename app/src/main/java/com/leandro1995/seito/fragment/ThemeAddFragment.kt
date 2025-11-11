4ba76b3b8f54cf612bb3b6a2c531152a6d484e11
LEANDRO\leccb <Leandro>
2025-11-10 20:33:16 -0500

    Obtener datos del curso para listar temas

--- app/src/main/java/com/leandro1995/seito/fragment/ThemeAddFragment.kt
+++ app/src/main/java/com/leandro1995/seito/fragment/ThemeAddFragment.kt
@@ -2,8 +2,12 @@ package com.leandro1995.seito.fragment
 
 import androidx.fragment.app.viewModels
 import com.leandro1995.seito.R
+import com.leandro1995.seito.config.Setting
 import com.leandro1995.seito.databinding.FragmentThemeAddBinding
+import com.leandro1995.seito.extension.parcelable
 import com.leandro1995.seito.fragment.ambient.FragmentAmbient
+import com.leandro1995.seito.model.design.Toolbar
+import com.leandro1995.seito.model.entity.Course
 import com.leandro1995.seito.viewmodel.ThemeAddViewModel
 
 class ThemeAddFragment : FragmentAmbient<FragmentThemeAddBinding>() {
@@ -14,5 +18,20 @@ class ThemeAddFragment : FragmentAmbient<FragmentThemeAddBinding>() {
 
     override fun initView() {
         dataBinding?.themeAddViewModel = themeAddViewModel
+    }
+
+    override fun putExtra() {
+        Setting.COURSE_PUT_EXTRA.parcelable<Course>(activity = requireActivity())?.let {
+            themeAddViewModel.course = it
+        }
+
+        dataBinding?.let {
+            Toolbar(
+                materialToolbar = it.appBarBlueInclude.toolbar,
+                titleText = themeAddViewModel.course.name,
+                icArrow = R.drawable.ic_arrow_white,
+                isArrow = true
+            ).config { requireActivity().finish() }
+        }
     }
 }
--- app/src/main/java/com/leandro1995/seito/viewmodel/ThemeAddViewModel.kt
+++ app/src/main/java/com/leandro1995/seito/viewmodel/ThemeAddViewModel.kt
@@ -1,5 +1,9 @@
 package com.leandro1995.seito.viewmodel
 
+import com.leandro1995.seito.model.entity.Course
 import com.leandro1995.seito.viewmodel.ambient.ViewModelAmbient
 
-class ThemeAddViewModel : ViewModelAmbient<Any, Any>()
+class ThemeAddViewModel : ViewModelAmbient<Any, Any>() {
+
+    var course = Course()
+}
