36c6ff892dab57c8ef1b18b900d1633682b229b7
LEANDRO\leccb <Leandro>
2025-11-12 00:08:53 -0500

    Corregir metodo onclick especifico

--- app/src/main/java/com/leandro1995/seito/component/list/adapter/viewholder/SubThemeVerticalViewHolder.kt
+++ app/src/main/java/com/leandro1995/seito/component/list/adapter/viewholder/SubThemeVerticalViewHolder.kt
@@ -10,7 +10,7 @@ class SubThemeVerticalViewHolder(
 ) : RecyclerView.ViewHolder(itemSubThemeVerticalBinding.root) {
 
     init {
-        itemSubThemeVerticalBinding.root.setOnClickListener {
+        itemSubThemeVerticalBinding.themeMaterialCardView.setOnClickListener {
             listAmbientOnclick.onclick(bindingAdapterPosition)
         }
     }
