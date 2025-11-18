c34756f6450f27813e3704886f9821ef6a271345
LEANDRO\leccb <Leandro>
2025-11-17 23:27:27 -0500

    Colocar viewmodel para lista de preguntas

--- app/src/main/java/com/leandro1995/seito/activity/QuestionListActivity.kt
+++ app/src/main/java/com/leandro1995/seito/activity/QuestionListActivity.kt
@@ -1,10 +1,14 @@
 package com.leandro1995.seito.activity
 
+import androidx.activity.viewModels
 import com.leandro1995.seito.R
 import com.leandro1995.seito.activity.ambient.ActivityAmbient
 import com.leandro1995.seito.databinding.ActivityQuestionListBinding
+import com.leandro1995.seito.viewmodel.QuestionListViewModel
 
 class QuestionListActivity : ActivityAmbient<ActivityQuestionListBinding>() {
+
+    val questionListViewModel by viewModels<QuestionListViewModel>()
 
     override var idLayout: Int = R.layout.activity_question_list
 }
