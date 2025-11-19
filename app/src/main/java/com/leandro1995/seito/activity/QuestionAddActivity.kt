776d045bf83e8d170e96077cf0dc743c2aa006fb
LEANDRO\leccb <Leandro>
2025-11-19 01:57:26 -0500

    Mostrar titulo del toolbar

--- app/src/main/java/com/leandro1995/seito/activity/QuestionAddActivity.kt
+++ app/src/main/java/com/leandro1995/seito/activity/QuestionAddActivity.kt
@@ -4,6 +4,7 @@ import androidx.activity.viewModels
 import com.leandro1995.seito.R
 import com.leandro1995.seito.activity.ambient.ActivityAmbient
 import com.leandro1995.seito.databinding.ActivityQuestionAddBinding
+import com.leandro1995.seito.model.design.Toolbar
 import com.leandro1995.seito.viewmodel.QuestionAddViewModel
 
 class QuestionAddActivity : ActivityAmbient<ActivityQuestionAddBinding>() {
@@ -13,6 +14,14 @@ class QuestionAddActivity : ActivityAmbient<ActivityQuestionAddBinding>() {
     override var idLayout: Int = R.layout.activity_question_add
 
     override fun initView() {
-        dataBinding?.questionAddViewModel = questionAddViewModel
+        dataBinding?.apply {
+            questionAddViewModel = this@QuestionAddActivity.questionAddViewModel
+            Toolbar(
+                materialToolbar = appBarBlueInclude.toolbar,
+                idTitle = R.string.add_question_title,
+                icArrow = R.drawable.ic_arrow_white,
+                isArrow = true
+            ).config { finish() }
+        }
     }
 }
