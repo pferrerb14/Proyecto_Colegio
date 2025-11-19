a18ce2e1b32c813e8950d1dfbd1d8d04d5829321
LEANDRO\leccb <Leandro>
2025-11-19 00:01:51 -0500

    Crear modelo opcion para la lista de preguntas

--- app/src/main/java/com/leandro1995/seito/model/entity/Question.kt
+++ app/src/main/java/com/leandro1995/seito/model/entity/Question.kt
@@ -3,7 +3,7 @@ package com.leandro1995.seito.model.entity
 class Question(
     var imageUrl: String = "",
     var name: String = "",
-    var optionArrayList: ArrayList<String> = arrayListOf(),
+    var optionArrayList: ArrayList<Option> = arrayListOf(),
     var answer: String = "",
     val isType: Boolean = false,
     var coin: Int = -1
