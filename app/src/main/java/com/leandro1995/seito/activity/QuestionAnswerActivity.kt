4f060fb33a9c5093c10c1ad9c4b80d67d102f584
LEANDRO\leccb <Leandro>
2025-11-21 19:22:14 -0500

    Habilidar titulo de la ventana

--- app/src/main/java/com/leandro1995/seito/activity/QuestionAnswerActivity.kt
+++ app/src/main/java/com/leandro1995/seito/activity/QuestionAnswerActivity.kt
@@ -3,8 +3,20 @@ package com.leandro1995.seito.activity
 import com.leandro1995.seito.R
 import com.leandro1995.seito.activity.ambient.ActivityAmbient
 import com.leandro1995.seito.databinding.ActivityQuestionAnswerBinding
+import com.leandro1995.seito.model.design.Toolbar
 
 class QuestionAnswerActivity : ActivityAmbient<ActivityQuestionAnswerBinding>() {
 
     override var idLayout: Int = R.layout.activity_question_answer
+
+    override fun initView() {
+        dataBinding?.apply {
+            Toolbar(
+                materialToolbar = appBarBlueInclude.toolbar,
+                idTitle = R.string.answer_questions_title,
+                isArrow = true,
+                icArrow = R.drawable.ic_arrow_white
+            ).config { finish() }
+        }
+    }
 }
