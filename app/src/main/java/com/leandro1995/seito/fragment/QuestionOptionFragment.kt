2788fb0380791f9f80ea5e13ebaab647ec079268
LEANDRO\leccb <Leandro>
2025-11-22 17:11:02 -0500

    Diseñar y modificar el ambiente del fragmento

--- app/src/main/java/com/leandro1995/seito/fragment/QuestionOptionFragment.kt
+++ app/src/main/java/com/leandro1995/seito/fragment/QuestionOptionFragment.kt
@@ -1,19 +1,10 @@
 package com.leandro1995.seito.fragment
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.leandro1995.seito.R
+import com.leandro1995.seito.databinding.FragmentQuestionOptionBinding
+import com.leandro1995.seito.fragment.ambient.FragmentAmbient
 
-class QuestionOptionFragment : Fragment() {
+class QuestionOptionFragment : FragmentAmbient<FragmentQuestionOptionBinding>() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        return inflater.inflate(R.layout.fragment_question_option, container, false)
-    }
+    override var idLayout: Int = R.layout.fragment_question_option
 }
