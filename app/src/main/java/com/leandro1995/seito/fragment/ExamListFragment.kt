5a8a1826895eed1b909c7241bb53a7335672f313
LEANDRO\leccb <Leandro>
2025-11-26 02:31:42 -0500

    Crear seccion de lista de examenes

--- app/src/main/java/com/leandro1995/seito/fragment/ExamListFragment.kt
+++ app/src/main/java/com/leandro1995/seito/fragment/ExamListFragment.kt
@@ -1,18 +1,10 @@
 package com.leandro1995.seito.fragment
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.leandro1995.seito.R
+import com.leandro1995.seito.databinding.FragmentExamListBinding
+import com.leandro1995.seito.fragment.ambient.FragmentAmbient
 
-class ExamListFragment : Fragment() {
+class ExamListFragment : FragmentAmbient<FragmentExamListBinding>() {
 
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_exam_list, container, false)
-    }
+    override var idLayout: Int = R.layout.fragment_exam_list
 }
