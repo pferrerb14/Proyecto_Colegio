ca6761cf9a782ace2b09f386909e66abf86b2924
LEANDRO\leccb <Leandro>
2025-11-30 18:03:54 -0500

    Crear lista de examenes por profesor

---
+++ app/src/main/java/com/leandro1995/seito/component/list/adapter/viewholder/ExamVerticalViewHolder.kt
@@ -0,0 +1,16 @@
+package com.leandro1995.seito.component.list.adapter.viewholder
+
+import androidx.recyclerview.widget.RecyclerView
+import com.leandro1995.seito.component.list.config.callback.adapter.ambient.ListAmbientOnclick
+import com.leandro1995.seito.databinding.ItemExamVerticalBinding
+
+class ExamVerticalViewHolder(
+    val itemExamVerticalBinding: ItemExamVerticalBinding, val listAmbientOnclick: ListAmbientOnclick
+) : RecyclerView.ViewHolder(itemExamVerticalBinding.root) {
+
+    init {
+        itemExamVerticalBinding.examRemoveImage.setOnClickListener {
+            listAmbientOnclick.onclick(position = bindingAdapterPosition)
+        }
+    }
+}
