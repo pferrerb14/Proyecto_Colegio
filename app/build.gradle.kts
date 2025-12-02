e51d5c981bfa7731f45b1ddc6f74e3ee6580938b
LEANDRO\leccb <Leandro>
2025-12-01 21:43:14 -0500

    Modificar version al 1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.leandro1995.seito"
         minSdk = 23
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
