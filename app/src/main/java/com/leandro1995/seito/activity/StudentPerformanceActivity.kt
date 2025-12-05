3af87d1003b81d499a7ee00cf8f0f5cddb27c786
LEANDRO\leccb <Leandro>
2025-12-04 20:47:34 -0500

    Colocar tab en ver promedio del alumno

--- app/src/main/java/com/leandro1995/seito/activity/StudentPerformanceActivity.kt
+++ app/src/main/java/com/leandro1995/seito/activity/StudentPerformanceActivity.kt
@@ -1,21 +1,10 @@
 package com.leandro1995.seito.activity
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.leandro1995.seito.R
+import com.leandro1995.seito.activity.ambient.ActivityAmbient
+import com.leandro1995.seito.databinding.ActivityStudentPerformanceBinding
 
-class StudentPerformanceActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_student_performance)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+class StudentPerformanceActivity : ActivityAmbient<ActivityStudentPerformanceBinding>() {
+
+    override var idLayout: Int = R.layout.activity_student_performance
 }
