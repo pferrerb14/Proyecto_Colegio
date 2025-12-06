7be18c29440ef88c085ecf875d142561473033de
LEANDRO\leccb <Leandro>
2025-12-06 06:48:33 -0500

    Configuranto el cargando por viewmodel

--- app/src/main/java/com/leandro1995/seito/viewmodel/ExamGraphicViewModel.kt
+++ app/src/main/java/com/leandro1995/seito/viewmodel/ExamGraphicViewModel.kt
@@ -1,7 +1,22 @@
 package com.leandro1995.seito.viewmodel
 
+import com.leandro1995.seito.component.model.Loading
 import com.leandro1995.seito.intent.action.ExamGraphicIntentAction
 import com.leandro1995.seito.intent.event.ExamGraphicIntentEvent
+import com.leandro1995.seito.intent.event.ambient.LoadingIntentEventAmbient
 import com.leandro1995.seito.viewmodel.ambient.ViewModelAmbient
 
-class ExamGraphicViewModel : ViewModelAmbient<ExamGraphicIntentAction, ExamGraphicIntentEvent>()
+class ExamGraphicViewModel : ViewModelAmbient<ExamGraphicIntentAction, ExamGraphicIntentEvent>() {
+
+    override fun loading(idService: Int, isDelayDisable: Boolean) {
+        emit(
+            event = ExamGraphicIntentEvent.Loading(
+                loadingIntentEventAmbient = LoadingIntentEventAmbient.Loading(
+                    loading = Loading(
+                        idService = idService, isDelayDisable = isDelayDisable
+                    )
+                )
+            )
+        )
+    }
+}
