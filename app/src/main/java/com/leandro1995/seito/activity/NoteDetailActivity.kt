f235335998df5e8445324f1553d4ab3e2533de21
LEANDRO\leccb <Leandro>
2025-12-07 03:49:58 -0500

    Maquetar pantalla de respuestas

---
+++ app/src/main/java/com/leandro1995/seito/activity/NoteDetailActivity.kt
@@ -0,0 +1,10 @@
+package com.leandro1995.seito.activity
+
+import com.leandro1995.seito.R
+import com.leandro1995.seito.activity.ambient.ActivityAmbient
+import com.leandro1995.seito.databinding.ActivityNoteDetailBinding
+
+class NoteDetailActivity : ActivityAmbient<ActivityNoteDetailBinding>() {
+
+    override var idLayout: Int = R.layout.activity_note_detail
+}
